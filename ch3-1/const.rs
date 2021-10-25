100a63f63c51b91419cc1563b181c1e10a9a8df7
IWATA JULIANA <mari.i@gainsline.com>
2021-10-25 15:51:18 +0900

    practice const

---
+++ ch3-1/const.rs
@@ -0,0 +1,12 @@
+fn main () {
+    const PI: i32 = 3;
+    let radius= 10;
+
+    // 円周率も計算
+    let cir = 2 * PI * radius;
+    println!("円周 = {}", cir );
+
+    let area = PI * radius * radius;
+    println!("面接 = {} " , area);
+    
+}
