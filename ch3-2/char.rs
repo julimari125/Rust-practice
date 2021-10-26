f02ab392fa922d88da5ac49db9f48d126051dc40
IWATA JULIANA <mari.i@gainsline.com>
2021-10-26 13:36:28 +0900

    char practice

---
+++ ch3-2/char.rs
@@ -0,0 +1,12 @@
+fn main () {
+    let c1:char = 'o';
+    let c2:char = 'h';
+    let c3:char = 'm';
+    let c4:char = '\n';
+    let c5 = 'R';
+    let c6 = 'U';
+    let c7 = 'S';
+    let c8 = 'T';
+
+    println!("{}{}{}{}{}{}{}{}", c1,c2,c3,c4,c5,c6,c7,c8);
+}
