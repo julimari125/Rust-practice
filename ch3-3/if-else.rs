b9e9cbe933b09cf2d218f81853b72af1a4f0ed20
IWATA JULIANA <mari.i@gainsline.com>
2021-10-26 14:29:59 +0900

    if  practice

---
+++ ch3-3/if-else.rs
@@ -0,0 +1,13 @@
+fn main () {
+    let x = 5;
+
+    if x > 10 {
+        println!("x = {}", x);
+        println!("xの値は10より大きいです");   
+    } else {
+        println!("x = {}", x);
+        println!("xの値は10以下です");
+    }
+
+    
+}
