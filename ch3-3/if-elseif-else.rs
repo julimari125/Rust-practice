d2c60b12164883b8b7921f27ee9c66b3c91932a4
IWATA JULIANA <mari.i@gainsline.com>
2021-10-26 14:39:41 +0900

    if-elseif-else  practice

---
+++ ch3-3/if-elseif-else.rs
@@ -0,0 +1,15 @@
+fn main () {
+    let score = 85;
+
+    if score >= 90 { 
+        println!("成績はAです");
+    } else if score >= 80{
+        println!("成績はBです");    
+    } else if score >= 70{
+        println!("成績はcです"); 
+    } else if score >= 60{
+        println!("成績はDです");
+    }else{
+        println!("成績はEです")
+    } 
+}   
